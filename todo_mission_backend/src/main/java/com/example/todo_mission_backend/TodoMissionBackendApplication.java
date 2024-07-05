92ccd9857431bce8b94221e04223a8b2b18f528a
alswp006 <alswp006@github.com>
2024-07-05 19:37:16 +0900

    Initial commit: backend setup exit

---
+++ todo_mission_backend/src/main/java/com/example/todo_mission_backend/TodoMissionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_mission_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMissionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMissionBackendApplication.class, args);
+	}
+
+}
